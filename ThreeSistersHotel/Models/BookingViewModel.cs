ee72d1bc8afd01363c2e37e3e974f36d2ba6d9ae
HA2909 <72696427+HA2909@users.noreply.github.com>
2020-10-21 00:30:53 +1100

    Hello

---
+++ ThreeSistersHotel/Models/BookingViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThreeSistersHotel.Models
+{
+    public class BookingViewModel
+    {
+        [Range(1,16)]
+        public int RoomID { get; set; }
+
+        [Display(Name = "Checking in Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}")]
+        public DateTime CheckIn { get; set; }
+
+        [Display(Name = "Checking out Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd'/'MM'/'yyyy}")]
+        public DateTime CheckOut { get; set; }
+
+       
+    }
+}
---
+++ ThreeSistersHotel/Pages/Bookings/BookARoom.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using ThreeSistersHotel.Models;
+
+namespace ThreeSistersHotel.Pages.Bookings
+{
+    public class BookARoomModel : PageModel
+    {
+        private readonly ThreeSistersHotel.Data.ThreeSistersHotelContext _context;
+
+        public BookARoomModel(ThreeSistersHotel.Data.ThreeSistersHotelContext context)
+        {
+            _context = context;
+        }
+        public BookingViewModel Booking { get; set; }
+
+        public IActionResult OnGet()
+        {
+            // Get the options for the MovieGoer select list from the database
+            // and save them in ViewData for passing to Content file
+            ViewData["Booking"] = new SelectList(_context.Booking, "CheckIn", "CheckOut");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+
+        }
+    }
+}
